dc7d05e1210551ec36f20dcc8f4c5492ec1cd242
Erik Zscheile <zseri.devel@ytrizja.de>
2020-02-13 16:59:54 +0100

    write tag indices

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 mod base;
 mod mangle;
 
-use std::{fs::File, io::Write, path::Path};
+use std::{collections::HashMap, fs::File, io::Write, path::Path};
 
 fn main() {
     use clap::Arg;
@@ -79,10 +79,10 @@ fn main() {
     };
 
     let mut ents = Vec::new();
+    let mut tagents = HashMap::<_, Vec<_>>::new();
+    let mut subents = HashMap::<_, Vec<_>>::new();
 
-    let mut subents = std::collections::HashMap::new();
-
-    base::tr_folder2(indir, &outdir, |fpath, rd: base::Post, mut wr| {
+    base::tr_folder2(indir, &outdir, |fpath, mut rd: base::Post, mut wr| {
         let (lnk, ret): (&str, bool) = match &rd.data {
             base::PostData::Link(ref l) => (&l, false),
             base::PostData::Text(ref t) => {
@@ -115,31 +115,31 @@ base::back_to_idx(fpath), &config.x_nav,
             }
         };
         let cdatef = rd.cdate.format("%d.%m.%Y");
-        ents.push(format!(
-            "{}: <a href=\"{}\">{}</a>",
-            &cdatef, lnk, &rd.title
-        ));
-        {
-            let fpap = Path::new(fpath);
-            if let Some(x) = fpap
-                .parent()
-                .and_then(|x| if x == null_path { None } else { Some(x) })
-            {
-                let bname = fpap.file_name().unwrap();
-                subents
-                    .entry(x.to_path_buf())
-                    .or_insert_with(Vec::new)
-                    .push(format!(
-                        "{}: <a href=\"{}\">{}</a>",
-                        &cdatef,
-                        if lnk == fpath {
-                            bname.to_str().unwrap()
-                        } else {
-                            lnk
-                        },
-                        &rd.title
-                    ));
+        let ent_str = format!("{}: <a href=\"{}\">{}</a>", &cdatef, lnk, &rd.title);
+        for i in std::mem::take(&mut rd.tags).into_iter() {
+            if is_valid_tag(&i) {
+                tagents.entry(i).or_default().push(ent_str.clone());
+            } else {
+                eprintln!("   - got invalid tag: {}", i);
             }
+        }
+        ents.push(ent_str);
+        let fpap = Path::new(fpath);
+        if let Some(x) = fpap
+            .parent()
+            .and_then(|x| if x == null_path { None } else { Some(x) })
+        {
+            let bname = fpap.file_name().unwrap();
+            subents.entry(x.to_path_buf()).or_default().push(format!(
+                "{}: <a href=\"{}\">{}</a>",
+                &cdatef,
+                if lnk == fpath {
+                    bname.to_str().unwrap()
+                } else {
+                    lnk
+                },
+                &rd.title
+            ));
         }
         ret
     });
@@ -160,9 +160,13 @@ base::back_to_idx(fpath), &config.x_nav,
         match i.parent() {
             None => &mut ents,
             Some(par) if par == null_path => &mut ents,
-            Some(par) => subents.entry(par.to_path_buf()).or_insert_with(Vec::new),
+            Some(par) => subents.entry(par.to_path_buf()).or_default(),
         }
         .push(format!("<a href=\"{}/index.html\">{}</a>", ibn, ibn));
+    }
+
+    for tag in tagents.keys() {
+        ents.push(format!("<a href=\"{}.html\">{}</a>", tag.replace('&', "&amp;"), tag));
     }
 
     write_index(&config, outdir, "", &ents).expect("unable to write main-index");
@@ -170,6 +174,18 @@ base::back_to_idx(fpath), &config.x_nav,
     for (subdir, p_ents) in subents.iter() {
         write_index(&config, outdir, subdir, &p_ents).expect("unable to write sub-index");
     }
+
+    for (tag, p_ents) in tagents.iter() {
+        write_tag_index(&config, outdir.as_ref(), tag, &p_ents).expect("unable to write tag-index");
+    }
+}
+
+fn is_valid_tag(tag: &str) -> bool {
+    !(tag.is_empty()
+        || tag.contains(|i| match i {
+            '.' | '/' | '\0' => true,
+            _ => false,
+        }))
 }
 
 fn write_index<P1, P2>(
@@ -246,6 +262,54 @@ fn write_index_inner(
 
     writeln!(&mut f, "</tt>\n  </body>\n</html>")?;
 
+    f.flush()?;
+    Ok(())
+}
+
+fn write_tag_index(
+    config: &base::Config,
+    outdir: &Path,
+    idx_name: &str,
+    ents: &[String],
+) -> std::io::Result<()> {
+    println!("- tag index: {}", &idx_name);
+
+    let mut fpath = Path::new(outdir).join(idx_name);
+    fpath.set_extension("html");
+    let mut f = std::io::BufWriter::new(std::fs::File::create(
+        fpath,
+    )?);
+
+    write!(
+        &mut f,
+        r#"<!doctype html>
+<html lang="de" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="{}" type="text/css" />
+    <title>Tag: {} &mdash; {}</title>
+{}  </head>
+  <body>
+    <h1>Tag: {} &mdash; {}</h1>
+{}
+<tt>
+<a href="index.html">[Hauptseite]</a><br />
+"#,
+        &config.stylesheet,
+        &idx_name,
+        &config.blog_name,
+        &config.x_head,
+        &idx_name,
+        &config.blog_name,
+        &config.x_body_ph1,
+    )?;
+
+    for i in ents.iter().rev() {
+        writeln!(&mut f, "{}<br />", i)?;
+    }
+
+    writeln!(&mut f, "</tt>\n  </body>\n</html>")?;
     f.flush()?;
     Ok(())
 }
