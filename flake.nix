fd670065bc3da7a0ba84edee2ef71c073ebd8e6a
zseri <zseri.devel@ytrizja.de>
2021-03-27 02:36:58 +0100

    +nix-flakify

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "a rust implementation of the 'crulz' macro language interpreter";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-20.09";
+    yz-flake-utils.url = "github:YZITE/flake-utils";
+    # needed for default.nix, shell.nix
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+  outputs = { nixpkgs, yz-flake-utils, ... }:
+    yz-flake-utils.lib.mkFlakeFromProg {
+      prevpkgs = nixpkgs;
+      progname = "zsstwebr";
+      drvBuilder = final: prev: (prev.pkgs.callPackage ./Cargo.nix {}).rootCrate.build;
+    };
+}
